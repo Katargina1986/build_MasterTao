98ed7001da34f81c5cb90308b8882cc6f9367f6c
Your Name <you@example.com>
2023-11-06 23:46:38 +0200

    first commit

---
+++ build/assets/js/custom.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+   
+    $('.first-screen-slider').slick({
+        dots: true,
+        appendArrows: '.first-screen-nav',
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                arrows: false,
+              }
+            },
+          ]
+    });
+
+const burger = document.querySelector('.hamburger');
+
+    burger.addEventListener('click', function () {
+        // burger.classList.replace("test-delete", "bar");
+        burger.classList.toggle('is-active')
+    })
+})
+
+document.getElementById('fileInput').onchange = function () {
+    document.getElementById('file-name').innerHTML = this.value;
+};
+     
+const checkButton = document.querySelectorAll('[type="checkbox"]');
+
+checkButton.forEach((item, index) => {
+  item.closest('.calculation-column').style.marginBottom = "-10px";
+  index === 0 ? item.closest('.input-box').classList.add('has-checkbox') : null;
+})
+
+ mobileOnlySlider("#advantages-slider", true, false, 991);
+
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+        var slider = $($slidername);
+        var settings = {
+            mobileFirst: true,
+            dots: $dots,
+            arrows: $arrows,
+            responsive: [{
+                breakpoint: $breakpoint,
+                settings: "unslick"
+            }]
+        };
+
+        slider.slick(settings);
+
+        $(window).on("resize", function() {
+            if ($(window).width() > $breakpoint) {
+                return;
+            }
+            if (!slider.hasClass("slick-initialized")) {
+                return slider.slick(settings);
+            }
+        });
+    }
